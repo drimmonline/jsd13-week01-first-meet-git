d9427f1aac647b15fec1b0efaf5206f1e8189a4c
Puttipong Runtakit <puttipong.runtakit@gmail.com>
2026-07-01 16:14:27 +0700

    add javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("เชื่อมต่อ Java script สำเร็จแล้ว");
+const title = "ยินดีต้อนรับเข้าสู่เว็บไซต์";
+
+const mybutton = document.getElementById("submit-btn");
+
+let grade = 80;
+if (grade >= 50 && grade < 55) {
+  console.log("Your Grade is D");
+} else if (grade >= 80) {
+  console.log("Your grade is A");
+}
